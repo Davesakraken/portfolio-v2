45f09d908b0e56abd69b39ba66e0f14dab6a257c
Davesakraken <davidgeorgebell1995@outlook.com>
2025-05-09 22:09:03 +0100

    test content and dark mode selector fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,12 @@ function HomePage() {
   return (
     <>
       <h1 className="">Home Page</h1>
+      <h2 className="text-2xl font-semibold mb-6">Welcome to my portfolio!</h2>
+      <p className="text-gray-600 dark:text-gray-400 mb-8 max-w-md">
+        This is a simple portfolio website built with React and Tailwind CSS. You can find my
+        projects and contact information here. Feel free to explore and reach out if you have any
+        questions or inquiries.
+      </p>
     </>
   );
 }
