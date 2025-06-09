7a65332addc0763aa51ad9fae84236ab6c3fe64a
Davesakraken <davidgeorgebell1995@outlook.com>
2025-06-09 22:05:49 +0100

    add GSAP and some basic animations

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,38 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
 function HomePage() {
+  const title = useRef<HTMLHeadingElement>(null);
+  const subtitle = useRef<HTMLHeadingElement>(null);
+
+  useGSAP(() => {
+    gsap.from(title.current, {
+      duration: 2,
+      opacity: 0,
+      y: 50,
+      ease: "power3.out",
+    });
+
+    gsap.from(subtitle.current, {
+      duration: 2,
+      delay: 0.5,
+      opacity: 0,
+      y: 50,
+      ease: "power3.out",
+    });
+  }, []);
+
   return (
     <>
-      <h1 className="">Home Page</h1>
-      <h2 className="text-2xl font-semibold mb-6">Welcome to my portfolio!</h2>
-      <p className="text-gray-600 dark:text-gray-400 mb-8 max-w-md">
-        This is a simple portfolio website built with React and Tailwind CSS. You can find my
-        projects and contact information here. Feel free to explore and reach out if you have any
-        questions or inquiries.
-      </p>
+      <div className="flex flex-col items-center mt-30">
+        <h1 ref={title} className="text-[5rem] font-bold">
+          David Bell
+        </h1>
+        <h2 ref={subtitle} className="text-2xl font-semibold mb-6">
+          デビッド
+        </h2>
+      </div>
     </>
   );
 }
